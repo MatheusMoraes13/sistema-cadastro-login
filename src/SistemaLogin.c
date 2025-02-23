b3564830e750e5a7a9255f768412beddd888e54d
Matheus Moraes <profissional.moraes2004@gmail.com>
2025-02-23 14:32:40 -0300

    AddFuntionButtonAtualizar/Pesquisar

--- src/SistemaLogin.c
+++ src/SistemaLogin.c
@@ -24,8 +24,7 @@ void mensagem (const char *text, const char *secondary_text, char *icon_name)
 
     g_object_set (mensagem_dialogo, "text", text, NULL);
     g_object_set (mensagem_dialogo, "secondary_text", secondary_text, NULL);
-    g_object_set (mensagem_dialogo, "icon_name", icon_name, NULL);
-
+    gtk_window_set_icon_from_file(GTK_WINDOW(mensagem_dialogo), icon_name, NULL);
 
     gtk_window_set_transient_for(GTK_WINDOW(mensagem_dialogo), GTK_WINDOW(window));
     gtk_widget_show_all (GTK_WIDGET (mensagem_dialogo));
@@ -41,16 +40,15 @@ void Login (const char *email, const char *senha)
     if ((strcmp (email, "admin") == 0) && (strcmp (senha, "admin") == 0))
     {
 
-        mensagem ("Bem Vindo", "Usuario logado com sucesso!", "emblem-default");
+        mensagem ("Bem Vindo", "Usuario logado com sucesso!", "icons/login-94x94.png");
         gtk_stack_set_visible_child_name (stack, "view_inicial");
-        g_object_set (window, "icon_name", "avatar-default-symbolic", NULL);
 
     }
 
     else
     {
 
-        mensagem ("Aviso", "Email ou senhas incorretos! \n tente novamente", "dialog-error");
+        mensagem ("Aviso", "Email ou senhas incorretos! \n tente novamente", "icons/warning-100x100.png");
 
     }
 }
@@ -89,7 +87,6 @@ void on_button_listar_inicial_clicked (GtkWidget *widget, gpointer data)
 void on_button_sair_inicial_clicked (GtkWidget *widget, gpointer data)
 {
 
-    g_object_set (window, "icon_name", "changes-prevent", NULL);
     gtk_stack_set_visible_child_name (stack, "view_login");
 
 }
@@ -154,24 +151,10 @@ void on_button_cadastrar_clicked (GtkWidget *widget, gpointer data)
     const char *cad_email = gtk_entry_get_text (entry_email);
     const char *cad_senha = gtk_entry_get_text (entry_senha);
 
-    if (strcmp (cad_nome, "") == 0)
+    if (strcmp (cad_nome, "") == 0 || strcmp (cad_email, "") == 0 || strcmp (cad_senha, "") == 0)
     {
 
-        mensagem ("Aviso", "Campo \"Nome\" obrigatorio!", "dialog-error");
-
-    }
-
-    if (strcmp (cad_email, "") == 0)
-    {
-
-        mensagem ("Aviso", "Campo \"Email\" obrigatorio!", "dialog-error");
-
-    }
-
-    if (strcmp (cad_senha, "") == 0)
-    {
-
-        mensagem ("Aviso", "Campo \"Senha\" obrigatorio!", "dialog-error");
+        mensagem ("Aviso", "Campo obrigatorio em branco!", "icons/warning-100x100.png");
 
     }
 
@@ -188,7 +171,7 @@ void on_button_cadastrar_clicked (GtkWidget *widget, gpointer data)
         rc = sqlite3_exec(db, insert, sqlite_retorno, 0, &mensagem_erro);
 
         if (rc != SQLITE_OK) {
-        mensagem("Erro", mensagem_erro, "dialog-error");
+        mensagem("Erro", mensagem_erro, "icons/warning-100x100.png");
         sqlite3_free(mensagem_erro);
         return;
         }
@@ -196,10 +179,11 @@ void on_button_cadastrar_clicked (GtkWidget *widget, gpointer data)
 
         char texto[128];
         g_snprintf(texto, 128, "%s%s%s", "Usuario ", cad_nome, " cadastrado!");
-        mensagem ("Aviso", texto, "emblem-default");
+        mensagem ("Aviso", texto, "icons/check_circle-100x100.png");
 
     }
 
+    sqlite3_close(db);
 }
 
 
@@ -230,20 +214,138 @@ void on_button_atualizar_login_clicked (GtkWidget *widget, gpointer data)
 void on_button_apagar_login_clicked (GtkWidget *widget, gpointer data)
 {
 
-    gtk_stack_set_visible_child_name (stack, "view_inicial");
+    gtk_stack_set_visible_child_name (stack, "view_apagar");
 
-}
-
-
-void on_button_atualizar_atualizar_clicked (GtkWidget *widget, gpointer data)
-{
-    gtk_stack_set_visible_child_name (stack, "view_modificar");
 }
 
 
 void on_button_voltar_atualizar_clicked (GtkWidget *widget, gpointer data)
 {
     gtk_stack_set_visible_child_name (stack, "view_modificar");
+}
+
+
+void on_button_pesquisar_atualizar_clicked (GtkWidget *widget, gpointer data)
+{
+
+    sqlite3 * db = 0;
+    sqlite3_stmt *stmt;
+    int rc = sqlite3_open("Logins.db3", &db);
+
+    GtkEntry *entry_pesquisar_atualizar = GTK_ENTRY (gtk_builder_get_object(builder, "pesquisa_atualizar"));
+    const char *pesquisar_atualizar = gtk_entry_get_text(entry_pesquisar_atualizar);
+
+    char *output;
+    long modify = strtol (pesquisar_atualizar, &output, 10);
+    int id = modify;
+
+    char *mensagem_erro = NULL;
+
+        if (rc != SQLITE_OK)
+        {
+
+            mensagem("Erro", mensagem_erro, "icons/warning-100x100.png");
+            sqlite3_free (mensagem_erro);
+            return;
+
+        }
+
+    char *query = "SELECT nome, email, senha FROM logins WHERE id = ?";
+
+    rc = sqlite3_prepare_v2 (db, query, -1, &stmt, NULL);
+    sqlite3_bind_int (stmt, 1, id);
+
+    GtkEntry *entry_nome_atualizar =  GTK_ENTRY (gtk_builder_get_object(builder, "nome_atualizar"));
+    GtkEntry *entry_email_atualizar = GTK_ENTRY (gtk_builder_get_object(builder, "email_atualizar"));
+    GtkEntry *entry_senha_atualizar = GTK_ENTRY (gtk_builder_get_object(builder, "senha_atualizar"));
+
+    if (sqlite3_step(stmt) == SQLITE_ROW)
+        {
+        const char *nome  = (const char *)sqlite3_column_text(stmt, 0);
+        const char *email = (const char *)sqlite3_column_text(stmt, 1);
+        const char *senha = (const char *)sqlite3_column_text(stmt, 2);
+
+        gtk_entry_set_text (entry_nome_atualizar,  nome);
+        gtk_entry_set_text (entry_email_atualizar, email);
+        gtk_entry_set_text (entry_senha_atualizar, senha);
+        }
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+}
+
+
+void on_button_atualizar_atualizar_clicked (GtkWidget *widget, gpointer data)
+{
+    sqlite3 * db = 0;
+    sqlite3_stmt *stmt;
+    int rc = sqlite3_open("Logins.db3", &db);
+    const char *query = "UPDATE logins SET nome = ?, email = ?, senha = ? WHERE id = ?";
+
+    GtkEntry *entry_pesquisar_atualizar = GTK_ENTRY (gtk_builder_get_object(builder, "pesquisa_atualizar"));
+    const char *pesquisar_atualizar = gtk_entry_get_text(entry_pesquisar_atualizar);
+    char *output;
+    long modify = strtol (pesquisar_atualizar, &output, 10);
+    int id = modify;
+
+    GtkEntry *entry_nome_atualizar  = GTK_ENTRY (gtk_builder_get_object(builder, "nome_atualizar"));
+    GtkEntry *entry_email_atualizar = GTK_ENTRY (gtk_builder_get_object(builder, "email_atualizar"));
+    GtkEntry *entry_senha_atualizar = GTK_ENTRY (gtk_builder_get_object(builder, "senha_atualizar"));
+
+    const char *nome_atualizar = gtk_entry_get_text(entry_nome_atualizar);
+    const char *email_atualizar = gtk_entry_get_text(entry_email_atualizar);
+    const char *senha_atualizar = gtk_entry_get_text(entry_senha_atualizar);
+
+    rc = sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
+    if (rc != SQLITE_OK) {
+        mensagem("Erro", "Erro ao preparar a consulta SQL.", "icons/warning-100x100.png");
+        return;
+    }
+
+    sqlite3_bind_text(stmt, 1, nome_atualizar,  -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, email_atualizar, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 3, senha_atualizar, -1, SQLITE_STATIC);
+    sqlite3_bind_int(stmt, 4, id);
+
+    rc = sqlite3_step (stmt);
+
+    char texto[128];
+    if (rc == SQLITE_DONE)
+    {
+        g_snprintf(texto, 128, "%s%s%s", "Usuario ", nome_atualizar, " atualizado!");
+        mensagem ("Aviso", texto, "icons/check_circle-100x100.png");
+    }
+
+    else
+    {
+        mensagem("Erro", "Falha ao atualizar o usuário!", "icons/warning-100x100.png");
+    }
+
+    sqlite3_finalize (stmt);
+    sqlite3_close (db);
+}
+
+
+void on_button_pesquisar_apagar_clicked (GtkWidget *widget, gpointer data)
+{
+
+    gtk_stack_set_visible_child_name (stack, "view_modificar");
+
+}
+
+
+void on_button_apagar_apagar_clicked (GtkWidget *widget, gpointer data)
+{
+
+    gtk_stack_set_visible_child_name (stack, "view_modificar");
+
+}
+
+
+void on_button_voltar_apagar_clicked (GtkWidget *widget, gpointer data)
+{
+
+    gtk_stack_set_visible_child_name (stack, "view_modificar");
+
 }
 
 
@@ -276,9 +378,9 @@ void on_button_listar_clicked (GtkWidget *widget, gpointer data)
     while(sqlite3_step(stmt) == SQLITE_ROW)
     {
         int id = sqlite3_column_int(stmt, 0);
-        const char *nome = (const char *)sqlite3_column_text(stmt, 1);
-        const char*email = (const char *)sqlite3_column_text(stmt, 2);
-        const char*senha = (const char *)sqlite3_column_text(stmt, 3);
+        const char *nome  = (const char *)sqlite3_column_text(stmt, 1);
+        const char *email = (const char *)sqlite3_column_text(stmt, 2);
+        const char *senha = (const char *)sqlite3_column_text(stmt, 3);
         gtk_list_store_append(modelo_armazenamento, &iter);
         gtk_list_store_set(modelo_armazenamento, &iter ,
                            0, id,
@@ -287,6 +389,9 @@ void on_button_listar_clicked (GtkWidget *widget, gpointer data)
                            3, senha,
                            -1);
     }
+
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
 }
 
 
@@ -300,20 +405,20 @@ void on_button_listar_voltar_clicked (GtkWidget *widget, gpointer data)
 
 int main (int argc, char *argv[])
 {
-    //Criando e abrindo o banco de dados
+
     char *mensagem_erro = NULL;
     sqlite3 * db = 0;
 
     int rc = sqlite3_open("Logins.db3", &db);
 
     if (rc != SQLITE_OK){
-        mensagem("Erro", "Falha ao abrir o banco de dados!", "dialog-error");
+        mensagem("Erro", "Falha ao abrir o banco de dados!", "icons/warning-100x100.png");
         sqlite3_close(db);
         return 1;
     }
 
     char create[] = "CREATE TABLE logins( "
-                    "id INTEGER PRIMARY KEY, "
+                    "id INTEGER PRIMARY KEY,"
                     "nome TEXT,"
                     "email TEXT,"
                     "senha TEXT)";
@@ -327,23 +432,27 @@ int main (int argc, char *argv[])
 
     gtk_builder_add_callback_symbols(
         builder,
-        "on_button_login_clicked",                        G_CALLBACK (on_button_login_clicked),
-        "on_main_window_destroy",                         G_CALLBACK (on_main_window_destroy),
-        "on_button_modificar_inicial_clicked",            G_CALLBACK (on_button_modificar_inicial_clicked),
-        "on_button_listar_inicial_clicked",               G_CALLBACK (on_button_listar_inicial_clicked),
-        "on_button_sair_inicial_clicked",                 G_CALLBACK (on_button_sair_inicial_clicked),
-        "on_button_cadastrar_clicked",                    G_CALLBACK (on_button_cadastrar_clicked),
-        "on_button_cad_voltar_clicked",                   G_CALLBACK (on_button_cad_voltar_clicked),
-        "on_button_listar_clicked",                       G_CALLBACK (on_button_listar_clicked),
-        "on_button_listar_voltar_clicked",                G_CALLBACK (on_button_listar_voltar_clicked),
-        "on_button_voltar_login_clicked",                 G_CALLBACK (on_button_voltar_login_clicked),
-        "on_button_apagar_login_clicked",                 G_CALLBACK (on_button_apagar_login_clicked),
-        "on_button_atualizar_login_clicked",              G_CALLBACK (on_button_atualizar_login_clicked),
-        "on_button_cadastrar_login_clicked",              G_CALLBACK (on_button_cadastrar_login_clicked),
-        "on_button_atualizar_login_clicked",              G_CALLBACK (on_button_atualizar_login_clicked),
-        "on_button_apagar_login_clicked",                 G_CALLBACK (on_button_apagar_login_clicked),
-        "on_button_atualizar_atualizar_clicked",          G_CALLBACK (on_button_atualizar_atualizar_clicked),
-        "on_button_voltar_atualizar_clicked",          G_CALLBACK (on_button_voltar_atualizar_clicked),
+        "on_button_login_clicked",                  G_CALLBACK (on_button_login_clicked),
+        "on_main_window_destroy",                   G_CALLBACK (on_main_window_destroy),
+        "on_button_modificar_inicial_clicked",      G_CALLBACK (on_button_modificar_inicial_clicked),
+        "on_button_listar_inicial_clicked",         G_CALLBACK (on_button_listar_inicial_clicked),
+        "on_button_sair_inicial_clicked",           G_CALLBACK (on_button_sair_inicial_clicked),
+        "on_button_cadastrar_clicked",              G_CALLBACK (on_button_cadastrar_clicked),
+        "on_button_cad_voltar_clicked",             G_CALLBACK (on_button_cad_voltar_clicked),
+        "on_button_listar_clicked",                 G_CALLBACK (on_button_listar_clicked),
+        "on_button_listar_voltar_clicked",          G_CALLBACK (on_button_listar_voltar_clicked),
+        "on_button_voltar_login_clicked",           G_CALLBACK (on_button_voltar_login_clicked),
+        "on_button_apagar_login_clicked",           G_CALLBACK (on_button_apagar_login_clicked),
+        "on_button_atualizar_login_clicked",        G_CALLBACK (on_button_atualizar_login_clicked),
+        "on_button_cadastrar_login_clicked",        G_CALLBACK (on_button_cadastrar_login_clicked),
+        "on_button_atualizar_login_clicked",        G_CALLBACK (on_button_atualizar_login_clicked),
+        "on_button_atualizar_atualizar_clicked",    G_CALLBACK (on_button_atualizar_atualizar_clicked),
+        "on_button_voltar_atualizar_clicked",       G_CALLBACK (on_button_voltar_atualizar_clicked),
+        "on_button_pesquisar_atualizar_clicked",    G_CALLBACK (on_button_pesquisar_atualizar_clicked),
+        "on_button_voltar_atualizar_clicked",       G_CALLBACK (on_button_voltar_atualizar_clicked),
+        "on_button_pesquisar_apagar_clicked",       G_CALLBACK (on_button_pesquisar_apagar_clicked),
+        "on_button_apagar_apagar_clicked",          G_CALLBACK (on_button_apagar_apagar_clicked),
+        "on_button_voltar_apagar_clicked",          G_CALLBACK (on_button_voltar_apagar_clicked),
         NULL);
 
     gtk_builder_connect_signals (builder, NULL);
