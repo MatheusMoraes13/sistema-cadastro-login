19f25e10f6be5cf03970c92d374ced65bf374195
MatheusMoraes13 <profissional.moraes2004@gmail.com>
2025-06-04 21:50:53 -0300

    Update: adicionando o sistema de login

--- sistema-cadastro-logins-front/src/app/pages/login/login.ts
+++ sistema-cadastro-logins-front/src/app/pages/login/login.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { DefaultLoginLayout } from '../../components/default-login-layout/default-login-layout';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInput } from '../../components/primary-input/primary-input';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [
     DefaultLoginLayout,
     ReactiveFormsModule,
-    PrimaryInput
+    PrimaryInput,
+    HttpClientModule
   ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
@@ -16,11 +20,20 @@ import { PrimaryInput } from '../../components/primary-input/primary-input';
 export class Login {
   loginForm!: FormGroup;
 
-  constructor(){
+  constructor(private http: HttpClient){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
     })
+  }
+
+  submit(){
+    const credentials = this.loginForm.value;
+    this.http.post('http://localhost:8080/auth/login', credentials)
+      .subscribe({
+        next: (data) => console.log('Login realizado:', data),
+        error: (err) => console.error('Erro no login:', err)
+      });
   }
 
 }
